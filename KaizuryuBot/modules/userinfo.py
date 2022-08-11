8e908bac13438575b0eba905097620295ad63f43
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2022-08-11 19:33:53 +0530

    Update userinfo.py

--- KaizuryuBot/modules/userinfo.py
+++ KaizuryuBot/modules/userinfo.py
@@ -532,7 +532,7 @@ GET_BIO_HANDLER = DisableAbleCommandHandler("bio", about_bio, run_async=True)
 STATS_HANDLER = CommandHandler("stats", stats, run_async=True)
 ID_HANDLER = DisableAbleCommandHandler("id", get_id, run_async=True)
 GIFID_HANDLER = DisableAbleCommandHandler("gifid", gifid, run_async=True)
-INFO_HANDLER = DisableAbleCommandHandler(("info", "book", run_async=True), info, run_async=True)
+INFO_HANDLER = DisableAbleCommandHandler(("info", "book"), info, run_async=True)
 
 SET_ABOUT_HANDLER = DisableAbleCommandHandler("setme", set_about_me, run_async=True)
 GET_ABOUT_HANDLER = DisableAbleCommandHandler("me", about_me, run_async=True)
