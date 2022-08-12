635a146f7f78d80bdef95dacd538da70c341f71a
Xelcius <mparashar210@gmail.com>
2022-08-12 06:43:29 +0530

    Update cust_filters.py

--- KaizuryuBot/modules/cust_filters.py
+++ KaizuryuBot/modules/cust_filters.py
@@ -414,7 +414,7 @@ def reply_filter(update, context):
                             caption=markdown_to_html(filtext),
                             reply_to_message_id=message.message_id,
                             parse_mode=ParseMode.HTML,
-                            disable_web_page_preview=True,
+
                             reply_markup=keyboard,
                         )
                     except BadRequest:
@@ -446,7 +446,7 @@ def reply_filter(update, context):
                             update.effective_message,
                             filt.reply,
                             parse_mode=ParseMode.MARKDOWN,
-                            disable_web_page_preview=True,
+
                             reply_markup=keyboard,
                         )
                     except BadRequest as excp:
@@ -467,7 +467,7 @@ def reply_filter(update, context):
                                     chat.id,
                                     filt.reply,
                                     parse_mode=ParseMode.MARKDOWN,
-                                    disable_web_page_preview=True,
+   
                                     reply_markup=keyboard,
                                 )
                             except BadRequest as excp:
