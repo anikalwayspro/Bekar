f1ea01cd642aebedf59a2ad6e8c4e0b583fda18b
VIVEK <99870677+Roninopp@users.noreply.github.com>
2023-06-29 22:38:21 +0530

    Update misc.py

--- KaizuryuBot/modules/helper_funcs/misc.py
+++ KaizuryuBot/modules/helper_funcs/misc.py
@@ -90,7 +90,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                 EqInlineKeyboardButton(
                     "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                EqInlineKeyboardButton("• ʜᴏᴍᴇ •", callback_data="kaizuryu_back"),
+                EqInlineKeyboardButton("𖣘Home𖣘", callback_data="kaizuryu_back"),
                 EqInlineKeyboardButton(
                     "▷", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
@@ -98,7 +98,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         ]
 
     else:
-        pairs += [[EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="kaizuryu_back")]]
+        pairs += [[EqInlineKeyboardButton("𖣘Back𖣘", callback_data="kaizuryu_back")]]
 
     return pairs
 
